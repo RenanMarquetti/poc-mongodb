3d690220a0661963846905284bd65bb1c27f8a68
RenanMarquetti <codeum.desenvolvimento@gmail.com>
2025-06-26 09:06:06 -0300

    update host connection mongo

--- src/00_connection.ts
+++ src/00_connection.ts
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 export let db;
 
 export const connect = async () => {
-	await mongoose.connect("mongodb://127.0.0.1:28017/pocmongo?directConnection=true&serverSelectionTimeoutMS=2000&appName=scriptpocmongo");
+	const host = "mongodb://localhost:27017/pocmongo"
+	await mongoose.connect(host);
 	db = mongoose.connection.db;
 	await db.dropDatabase();
 }
